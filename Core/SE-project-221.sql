efcba22cea85307ce49bf477440f8c0a5a446403
nqduy65 <nguyenquocduy06051998@gmail.com>
2022-12-16 10:55:46 +0700

    update db

--- Core/SE-project-221.sql
+++ Core/SE-project-221.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2022 at 11:43 AM
+-- Generation Time: Dec 16, 2022 at 03:16 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `addresslist` (
-  `diachi_id` int(10) UNSIGNED NOT NULL,
-  `diachi_name` char(255) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` char(255) NOT NULL,
   `ward` char(255) NOT NULL,
   `district` char(100) NOT NULL,
   `diachi_phone` decimal(10,0) DEFAULT NULL
@@ -42,9 +42,17 @@ CREATE TABLE `addresslist` (
 --
 
 CREATE TABLE `category` (
-  `category_id` int(10) UNSIGNED NOT NULL,
-  `category_name` varchar(255) NOT NULL
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(1, 'Rau'),
+(2, 'Nước uống');
 
 -- --------------------------------------------------------
 
@@ -81,12 +89,23 @@ CREATE TABLE `employee` (
 --
 
 CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `order_phone` decimal(10,0) UNSIGNED NOT NULL,
+  `price` float UNSIGNED NOT NULL,
+  `diachi_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders_detail`
+--
+
+CREATE TABLE `orders_detail` (
   `orders_id` int(10) UNSIGNED NOT NULL,
-  `orders_phone` decimal(10,0) NOT NULL,
-  `orders_product_id` int(10) UNSIGNED NOT NULL,
+  `product_id` int(10) UNSIGNED NOT NULL,
   `product_qt` int(11) NOT NULL,
-  `text` varchar(500) NOT NULL,
-  `orders_diachi` char(255) NOT NULL
+  `price` double UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -96,19 +115,19 @@ CREATE TABLE `orders` (
 --
 
 CREATE TABLE `products` (
-  `product_id` int(10) UNSIGNED NOT NULL,
-  `product_name` char(255) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` char(255) NOT NULL,
   `description` char(255) DEFAULT NULL,
   `price` int(10) UNSIGNED NOT NULL,
-  `img` varchar(500) DEFAULT NULL,
-  `prod_category` int(10) UNSIGNED DEFAULT NULL
+  `imgURL` varchar(500) DEFAULT NULL,
+  `product_category` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `img`, `prod_category`) VALUES
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `imgURL`, `product_category`) VALUES
 (1, 'rau muống xào', 'rau muống xào với tỏi', 35500, '', NULL),
 (2, 'rau muống luộc', NULL, 30000, NULL, NULL);
 
@@ -120,14 +139,14 @@ INSERT INTO `products` (`product_id`, `product_name`, `description`, `price`, `i
 -- Indexes for table `addresslist`
 --
 ALTER TABLE `addresslist`
-  ADD PRIMARY KEY (`diachi_id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `address_fk_1` (`diachi_phone`);
 
 --
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `customer`
@@ -145,16 +164,22 @@ ALTER TABLE `employee`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`orders_id`) USING BTREE,
-  ADD KEY `order_fk_1` (`orders_phone`),
-  ADD KEY `order_fk_2` (`orders_product_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_fk_1` (`diachi_id`);
+
+--
+-- Indexes for table `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  ADD PRIMARY KEY (`orders_id`,`product_id`) USING BTREE,
+  ADD KEY `order_fk_2` (`product_id`);
 
 --
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `products_fk_01` (`prod_category`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_fk_01` (`product_category`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -164,13 +189,13 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `addresslist`
 --
 ALTER TABLE `addresslist`
-  MODIFY `diachi_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `employee`
@@ -182,13 +207,19 @@ ALTER TABLE `employee`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders_detail`
+--
+ALTER TABLE `orders_detail`
   MODIFY `orders_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -204,14 +235,19 @@ ALTER TABLE `addresslist`
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `order_fk_1` FOREIGN KEY (`orders_phone`) REFERENCES `customer` (`phone`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `order_fk_2` FOREIGN KEY (`orders_product_id`) REFERENCES `products` (`product_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `order_fk_1` FOREIGN KEY (`diachi_id`) REFERENCES `addresslist` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `orders_detail`
+--
+ALTER TABLE `orders_detail`
+  ADD CONSTRAINT `order_fk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `products`
 --
 ALTER TABLE `products`
-  ADD CONSTRAINT `products_fk_01` FOREIGN KEY (`prod_category`) REFERENCES `category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `products_fk_01` FOREIGN KEY (`product_category`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
