ca11552cd720e666dc524abdb11d95d6578ccff9
nqduy65 <nguyenquocduy06051998@gmail.com>
2022-12-16 23:40:46 +0700

    udpate Database.php

--- Core/Database.php
+++ Core/Database.php
@@ -7,7 +7,7 @@
         public function connect(){
             $connect = mysqli_connect(self::HOST, self::USERNAME,self::PASSWORD,self::DB_NAME);
             mysqli_set_charset($connect,'utf8');
-            if(mysqli_connect_errno($connect) === 0){
+            if(mysqli_connect_errno() === 0){
                 return $connect;
             }
             return false;
